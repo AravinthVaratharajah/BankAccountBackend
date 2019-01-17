cfd436e2029c8a9653895c2ead24f73740890974
Varatharajah <aravinth.varatharajah@yahoo.com>
2019-01-17 16:04:01 +0100

    #4 - BankAccountTest OK + Add NUnit3TestAdapter

---
+++ BankAccount/Backend/BankAccount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Backend
+{
+    public class BankAccount
+    {
+        public int accountId { get; set; }
+
+        public decimal balance { get; set; }
+
+        public BankAccount(int id)
+        {
+            accountId = id;
+            balance = 0.00m;
+        }
+
+        public decimal GetBalance (BankAccount account) => account.balance;
+    }
+}
