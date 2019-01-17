49deea9ef1f03a5e727863fd38905efcae7b634c
Varatharajah <aravinth.varatharajah@yahoo.com>
2019-01-17 16:16:36 +0100

    #5 - Transaction Test KO

---
+++ BankAccount/UnitTest/TransactionTest.cs
@@ -0,0 +1,31 @@
+﻿using Backend;
+using NFluent;
+using NUnit.Framework;
+
+namespace UnitTest
+{
+    [TestFixture]
+    public class TransactionTest
+    {
+        public BankAccount account { get; set; }
+
+
+        [TestCase(1, 100.00)]
+        [TestCase(2, 200.00)]
+        public void Should_Deposit_Money_In_Account(int accountId, decimal expectedBalance)
+        {
+            account = new BankAccount(accountId);
+            
+            Check.That(account.GetBalance(account)).IsEqualTo(expectedBalance);
+        }
+
+        [TestCase(1, 0.00)]
+        [TestCase(2, 100.00)]
+        public void Should_WithDraw_Money_In_Account(int accountId, decimal expectedBalance)
+        {
+            account = new BankAccount(accountId);
+
+            Check.That(account.GetBalance(account)).IsEqualTo(expectedBalance);
+        }
+    }
+}
