1fcafa1a7d63ff0e53c7d4e865e2eb962553eb3f
Varatharajah <aravinth.varatharajah@yahoo.com>
2019-01-17 16:33:33 +0100

    #6 - Transaction Test OK

---
+++ BankAccount/Backend/Transaction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Backend
+{
+    public class Transaction
+    {
+        public void Withdraw(BankAccount account, decimal amount)
+        {
+            account.balance -= amount;
+        }
+
+        public void Deposit(BankAccount account, decimal amount)
+        {
+            account.balance += amount;
+        }
+    }
+}
--- BankAccount/UnitTest/TransactionTest.cs
+++ BankAccount/UnitTest/TransactionTest.cs
@@ -8,22 +8,34 @@ namespace UnitTest
     public class TransactionTest
     {
         public BankAccount account { get; set; }
+        public Transaction transaction { get; set; }
 
+        [SetUp]
+        public void SetUp()
+        {
+            transaction = new Transaction();
+        }
 
         [TestCase(1, 100.00)]
         [TestCase(2, 200.00)]
         public void Should_Deposit_Money_In_Account(int accountId, decimal expectedBalance)
         {
             account = new BankAccount(accountId);
-            
+
+            transaction.Deposit(account, expectedBalance);
+
             Check.That(account.GetBalance(account)).IsEqualTo(expectedBalance);
         }
 
         [TestCase(1, 0.00)]
-        [TestCase(2, 100.00)]
         public void Should_WithDraw_Money_In_Account(int accountId, decimal expectedBalance)
         {
-            account = new BankAccount(accountId);
+            account = new BankAccount(accountId)
+            {
+                balance = 100.00m
+            };
+
+            transaction.Withdraw(account, 100);
 
             Check.That(account.GetBalance(account)).IsEqualTo(expectedBalance);
         }
