c548581a0bc9a3067a231e9bbffac5646d5fed29
Varatharajah <aravinth.varatharajah@yahoo.com>
2019-01-17 14:49:39 +0100

    #3 - Unit Test with NFluent et NUnit BankAccountTest KO

---
+++ BankAccount/Backend/BankAccountTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace Backend
+{
+    [TestFixture]
+    public class BankAccountTest
+    {
+        [TestCase(1, 100,00)]
+        [TestCase(2, 200,00)]
+        public void Should_Return_The_Balance_With_AccountId(int accountId, decimal balanceExpected)
+        {
+
+        }
+
+        [TestCase(0,00)]
+        public void Should_Create_New_Account_With_Zero_Balance(decimal balanceExpected)
+        {
+
+        }
+    }
+}
