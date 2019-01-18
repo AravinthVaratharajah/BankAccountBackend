bcf6b90a200c1ee2e555d172a6ae0e8fc37c695f
Unknown <aravinth.varatharajah@yahoo.com>
2019-01-18 16:34:48 +0100

    #16 - Refacto test's names and add Main program

--- BankAccount/Backend/Program.cs
+++ BankAccount/Backend/Program.cs
@@ -6,7 +6,50 @@ namespace Backend
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Account newaccount = new Account(1);
+            ITransaction transaction = new Transaction();
+            Console.WriteLine("Would you like to Deposit (D) or Withdraw (W) ?");
+            ConsoleKeyInfo info = Console.ReadKey();
+
+            if (info.Key == ConsoleKey.D)
+            {
+                Console.WriteLine("How much would you like to deposit ?");
+                var value = Console.ReadLine();
+                try
+                {
+                    int amount = int.Parse(value);
+                    transaction.Deposit(newaccount, amount);
+                    Console.WriteLine("Your new balance is " + newaccount.balance + "€");
+                    Console.ReadKey();
+                }
+                catch (Exception ex)
+                {
+                    if (ex.Message != null)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+
+            if (info.Key == ConsoleKey.W)
+            {
+                Console.WriteLine("How much would you like to withdraw ?");
+                var value = Console.ReadLine();
+                try
+                {
+                    int amount = int.Parse(value);
+                    transaction.Withdraw(newaccount, amount);
+                    Console.WriteLine("Your new balance is " + newaccount.balance + "€");
+                    Console.ReadKey();
+                }
+                catch (Exception ex)
+                {
+                    if(ex.Message != null)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
         }
     }
 }
--- BankAccount/UnitTest/BankAccountTest.cs
+++ BankAccount/UnitTest/BankAccountTest.cs
@@ -12,7 +12,7 @@ namespace UnitTest
             transaction = new Transaction();
         }
 
-        [TestCase(1, 100.00)]         [TestCase(2, 200.00)]         public void Should_Return_The_Balance_With_AccountId(int accountId, decimal expectedBalance)         {             account = new Account(accountId)             {                 balance = expectedBalance             };             Check.That(account.balance).IsEqualTo(expectedBalance);         }          [TestCase(0.00)]         public void Should_Create_New_Account_With_Zero_Balance(decimal expectedBalance)         {             account = new Account(1);             Check.That(account.balance).IsEqualTo(expectedBalance);         }
+        [TestCase(1, 100.00)]         [TestCase(2, 200.00)]         public void Should_Check_The_Account_Balance(int accountId, decimal expectedBalance)         {             account = new Account(accountId)             {                 balance = expectedBalance             };             Check.That(account.balance).IsEqualTo(expectedBalance);         }          [TestCase(0.00)]         public void Should_Create_New_Account_With_Zero_Balance(decimal expectedBalance)         {             account = new Account(1);             Check.That(account.balance).IsEqualTo(expectedBalance);         }
 
         [TestCase]
         public void Should_Return_Operation_List()
