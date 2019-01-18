68a04fd25b2405c3739c60048fbd6c9a2b35ce7b
Varatharajah <aravinth.varatharajah@yahoo.com>
2019-01-18 10:45:14 +0100

    #10 - Bank Account Test Add new Case for operations list

--- BankAccount/UnitTest/BankAccountTest.cs
+++ BankAccount/UnitTest/BankAccountTest.cs
@@ -4,5 +4,11 @@ using NUnit.Framework;
 
 namespace UnitTest
 {
-    [TestFixture]     public class BankAccountTest     {         public BankAccount account { get; set; }          [TestCase(1, 100.00)]         [TestCase(2, 200.00)]         public void Should_Return_The_Balance_With_AccountId(int accountId, decimal expectedBalance)         {             account = new BankAccount(accountId)             {                 balance = expectedBalance             };             Check.That(account.balance).IsEqualTo(expectedBalance);         }          [TestCase(0.00)]         public void Should_Create_New_Account_With_Zero_Balance(decimal expectedBalance)         {             account = new BankAccount(1);             Check.That(account.balance).IsEqualTo(expectedBalance);         }     } 
+    [TestFixture]     public class BankAccountTest     {         public BankAccount account { get; set; }          [TestCase(1, 100.00)]         [TestCase(2, 200.00)]         public void Should_Return_The_Balance_With_AccountId(int accountId, decimal expectedBalance)         {             account = new BankAccount(accountId)             {                 balance = expectedBalance             };             Check.That(account.balance).IsEqualTo(expectedBalance);         }          [TestCase(0.00)]         public void Should_Create_New_Account_With_Zero_Balance(decimal expectedBalance)         {             account = new BankAccount(1);             Check.That(account.balance).IsEqualTo(expectedBalance);         }
+
+        [TestCase]
+        public void Should_Return_Operation_List()
+        {
+
+        }     } 
 }
