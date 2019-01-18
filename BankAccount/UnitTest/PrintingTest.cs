7c04a3ee4432dad0e0350cee0a84e5666e1d8eb6
Varatharajah <aravinth.varatharajah@yahoo.com>
2019-01-18 11:52:02 +0100

    #13 - Printing Test KO

---
+++ BankAccount/UnitTest/PrintingTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Backend;
+using NFluent;
+using NUnit.Framework;
+
+
+namespace UnitTest
+{
+    [TestFixture]
+    public class PrintingTest
+    {
+        public Account account { get; set; }
+        public ITransaction transaction { get; set; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            account = new Account(1);
+            transaction = new Transaction();
+        }
+
+        [TestCase]
+        public void Should_Print_Operations()
+        {
+            transaction.Deposit(account, 100);
+            transaction.Withdraw(account, 200);
+        }
+    }
+}
